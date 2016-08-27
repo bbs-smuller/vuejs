8c28b878ed172575135b52fbcdc65c09cd2a0df7
Sébastien Muller <smuller@big-boss-studio.com>
2016-08-27 22:54:00 +0200

    Setup Vuex Counter default example

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,11 @@
-console.log('Hey!');
+import _ from 'lodash'
+import Vue from 'vue'
+import Counter from './components/Counter.vue'
+import store from './store'
+
+Vue.component('counter', Counter)
+
+const app = new Vue({
+    el: '#app',
+    store
+})
